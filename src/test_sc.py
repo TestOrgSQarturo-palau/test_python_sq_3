bbaa76ae7e7a8f797af65d20c0d9cc26d88a1e41
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2024-02-19 13:22:58 -0600

    Update test_sc.py

--- src/test_sc.py
+++ src/test_sc.py
@@ -3,9 +3,7 @@ changed = re.sub(r"Bob is", "It's", init) # Noncompliant
 changed = re.sub(r"\.\.\.", ";", changed) # Noncompliant
 
 
-n = 1
-while n < 10:
-    if n % 3 == 0:
-      print("Found a number divisible by 3", n)
-      break
-    n = n + 1
+narg=len(sys.argv)
+if narg == 1:
+    print('@Usage: input_filename nelements nintervals')
+    break
